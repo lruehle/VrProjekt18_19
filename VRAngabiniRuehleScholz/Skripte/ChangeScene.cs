2b6653e34a47d54bb88e95213c642aaa60056b28
lruehle <35702288+lruehle@users.noreply.github.com>
2019-03-01 17:58:40 +0100

    EndVersion

---
+++ VRAngabiniRuehleScholz/Skripte/ChangeScene.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    public Animator animator;
+    private int level;
+    public AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    //https://www.youtube.com/watch?v=Oadq-IrOazg // StartCoroutine();
+
+    // Update is called once per frame
+    void Update () {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            FadetoScene(1);
+        }*/
+	}
+    public void triggerFade0()
+    {
+        FadetoScene(0);
+    }
+    public void triggerFade1()
+    {
+            FadetoScene(1);    
+    }
+    public void triggerFade2()
+    {
+            FadetoScene(2);    
+    }
+    public void triggerFade3()
+    {
+        FadetoScene(3);
+    }
+    public void triggerFade4()
+    {
+        FadetoScene(4);
+    }
+
+    public void FadetoScene(int levelIndex)
+    {
+        level = levelIndex;
+        audio.enabled = true;
+        animator.SetTrigger("FadeOUt");
+    }
+
+    public void onFadeComplete()
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void EndGame()
+    {
+        Debug.Log("presssed Exit button");
+        Application.Quit();
+        
+    }
+}
---
+++ VRAngabiniRuehleScholz/Skripte/TeleportBeginn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBeginn : MonoBehaviour {
+    public ChangeScene ChangeScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            ChangeScene.triggerFade1();
+        }
+    }
+}
